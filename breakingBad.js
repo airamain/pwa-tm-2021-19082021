7909f6079dabed0966dc0617eb8cdc48df5d4d7e
Gianfranco-Buzzelatto <gianbuzzelattogmail.com>
2021-08-23 12:23:13 -0300

    Tarea realizada

--- breakingBad.js
+++ breakingBad.js
@@ -2,22 +2,36 @@ const axios = require('axios');
 
 const URL = 'https://www.breakingbadapi.com/api/characters'
 
+const get = (URL) => {
+    const data = axios.get(URL)
+        .then(res => console.log(res.data.results))
+        .catch(e => console.log(e));
+}
 
+const getAsync = async (URL) => {
+    try {
+        const data = await axios.get(URL);
+        return data;
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
 const getPersonajesNombre = async () => {
     try {
-        const personajes = await getAsync(URL);
-        let nombre = personajes.data.results.map(personaje => personaje.name);
-
+        const personajesA = await getAsync(URL);
+        const personajes = personajesA.data;
+        let nombre = personajes.map(personaje => personaje.name);
         console.log(nombre);
     }
     catch (error) {
         console.log(error);
     }
 }
-const getGenero = async (status = "Alive") => {
+const getStatus = async (status = "Deceased") => {
     try {
-        const info = await getAsync(URL);
-        const personajes = info.data.results;
+        const personajesA = await getAsync(URL);
+        const personajes = personajesA.data;
         const filtrado = personajes.filter(personaje => personaje.status === status);
         console.log(filtrado);
     }
@@ -26,5 +40,5 @@ const getGenero = async (status = "Alive") => {
     }
 }
 
-getGenero();
+getStatus();
 getPersonajesNombre();
