dd8a5f8d46f9b85ed84c6e8f82fa0acac517c682
Repolloso <agus.rodriguez777@gmail.com>
2021-08-23 16:26:04 -0300

    cambie el get genero y agregue un map con status

--- repaso2.js
+++ repaso2.js
@@ -1,27 +1,26 @@
 const axios = require('axios');
 
-// const URL = 'https://rickandmortyapi.com/api/character';
-const URL = 'https://www.breakingbadapi.com/api/characters'
+const URL = 'https://rickandmortyapi.com/api/character';
 
-const get = (url) => {
+/* const get = (url) => {
     const data = axios.get(url)
         .then(res => console.log(res.data.results))
         .catch(e => console.log(e));
     //return data;
     //console.log(data);
-}
+} */
 
-const getAsync = async (url) => {
+const getAsync = async (URL) => {
     try {
-        const data = await axios.get(url);
+        const data = await axios.get('https://rickandmortyapi.com/api/character');
         return data;
     }
     catch (error) {
         console.log(error)
     }
-}
+} 
 
-const getPersonajes = async () => {
+/* const getPersonajes = async () => {
     try {
         const personajes = await getAsync(URL);
         console.log(personajes.data);
@@ -30,7 +29,8 @@ const getPersonajes = async () => {
         console.log(error);
     }
     
-}
+} */
+/* 
 //map, filter
 const getPersonajesNombre = async () => {
     try {
@@ -38,28 +38,44 @@ const getPersonajesNombre = async () => {
         /*personajes.data.results.forEach(personaje => {
             nombreP.push(personaje.name);
         });*///esta es la forma menos eficiente
+
 //diferencia entre map y filtrer
 // map --> el array crea una nueva array y va haciendo push solo insertando datos dentro
 // filtrer --> 
-        let nombreP = personajes.data.results.map(personaje => personaje.name);
 
-        console.log(nombreP);
-    }
-    catch (error) {
-        console.log(error);
-    }
+/* let nombreP = personajes.data.results.map(personaje => personaje.name);
+
+    console.log(nombreP);
 }
-const getGenero = async (gender = "Female") => {
+catch (error) {
+    console.log(error);
+}
+}  */
+
+const getGenero = async (species = "Human") => {
     try {
-        const info = await getAsync(URL);
+        const info = await getAsync('https://rickandmortyapi.com/api/character');
         const personajes = info.data.results;
-        const filtrado = personajes.filter(personaje => personaje.gender === gender);
+        const filtrado = personajes.filter(especies => especies.species === species); 
         console.log(filtrado);
     }
     catch (e) {
         console.log(e);
     }
 }
+
+const getStatus = async () => {
+    try {
+        const info = await getAsync('https://rickandmortyapi.com/api/character')
+        let estados = info.data.results.map(estado => estado.status);
+        console.log(estados);
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+/*
 const getSingle = async (id) => {
     try {
         const info = await getAsync(`${URL}/${id}`); // URL + '/' + id
@@ -71,66 +87,71 @@ const getSingle = async (id) => {
     catch (e) {
         console.error(e);
     }
-
 }
+*/
+
 /* la API de rick y morty:
 Hacer un array con todos los personajes con status: "Alive" y species: "Human".
 Hacer otro array con todos los personajes con species: "Alien".
 
 En la pokeAPI:
 Conectarse a la pokeAPI, y hacer un get de todos los pokemons y hacer un getSingle.*/
-const getVivos = async (status = "Alive", species = "Human") => {
-    try {
-        const info = await getAsync(URL);
-        const personajes = info.data.results;
-        const vivos = personajes.filter(personaje => personaje.status === status && personaje.species === species);
-        console.log(vivos);
-    }
-    catch (e) {
-        console.error(e);
-    }
-}
-const getAlien = async (species = "Alien") => {
-    try {
-        const info = await getAsync(URL);
-        const personajes = info.data.results;
-        const aliens = personajes.filter(personaje => personaje.species === species);
-        console.log(aliens);
-    }
-    catch (e) {
-        console.error(e);
-    }
-}
+/* // const getVivos = async (status = "Alive", species = "Human") => {
+//     try {
+//         const info = await getAsync(URL);
+//         const personajes = info.data.results;
+//         const vivos = personajes.filter(personaje => personaje.status === status && personaje.species === species);
+//         console.log(vivos);
+//     }
+//     catch (e) {
+//         console.error(e);
+//     }
+// }
+// const getAlien = async (species = "Alien") => {
+//     try {
+//         const info = await getAsync(URL);
+//         const personajes = info.data.results;
+//         const aliens = personajes.filter(personaje => personaje.species === species);
+//         console.log(aliens);
+//     }
+//     catch (e) {
+//         console.error(e);
+//     }
+// } */
 
 
-const getPokemon = async () => {
-    try {
-        const info = await getAsync('https://pokeapi.co/api/v2/pokemon');
-        const pokemons = info.data.results;
-        console.log(pokemons);
-    }
-    catch (e) {
-        console.error(e);
-    }
-}
-const getPokemonSingle = async (id) => {
-    try {
-        const info = await getAsync(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const pokemon = info.data;
-        console.log(pokemon);
-    }
-    catch (e) {
-        console.error(e);
-    }
-}
+// const getPokemon = async () => {
+//     try {
+//         const info = await getAsync('https://pokeapi.co/api/v2/pokemon');
+//         const pokemons = info.data.results;
+//         console.log(pokemons);
+//     }
+//     catch (e) {
+//         console.error(e);
+//     }
+// }
+// const getPokemonSingle = async (id) => {
+//     try {
+//         const info = await getAsync(`https://pokeapi.co/api/v2/pokemon/${id}`);
+//         const pokemon = info.data;
+//         console.log(pokemon);
+//     }
+//     catch (e) {
+//         console.error(e);
+//     }
+// }                                     ctrl + / comenta el codigo en linea y alt + shift + A envelve el codigo en comentario /* */
 //cuando quiero consumir en otra parte del programa un dato de una funcion async await, lo tengo que consumir dentro de otra funcion async await.
-getPersonajes();
+// getPersonajes();
+// getMundo();
 // getSingle(1);
 // getVivos();
 // getAlien();
 // getPokemon();
 // getPokemonSingle(1);
-// getGenero();
+getGenero();
+getAsync();
+getStatus();
+// get();
 // getPersonajesNombre();
 
 //diferencia entre map y filtrer
