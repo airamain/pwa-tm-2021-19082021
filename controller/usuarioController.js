a3fbbbc7b2b81e65957fd783831b5af16b527c81
Jose Manuel Adrian Iramain <jiramain@todo1.net>
2021-09-07 10:21:28 -0300

    aplicamos findOne, salt y hasheo, express-validator

--- controller/usuarioController.js
+++ controller/usuarioController.js
@@ -1,13 +1,41 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 exports.crearUsuario = async (req, res) => {
-    console.log("body =>: ", req.body);
+    // vamos a extraer email y password - ok
+    // validar que el usuario registrado sea unico - ok
+    // para validad utilizaremos findOne(), buscamos un registro unico -ok
+    // retornar una respuesta desde nuestra API si es o no verdadera la validacion -ok
+
+    // con el dato del password, vamos a encriptarlo o hashearlo con "npm i bcryptjs "
+    //--------------------------//
+    
+    // aplicamos express-validator 
+    const error = validationResult(req);
+
+    if (!error.isEmpty()) {
+        return res.status(400).json({ errores: error.array() })
+    }
+
     try {
-        let usuario;
+        const { email, password } = req.body;
+
+        let usuario = await Usuario.findOne({ email });
+
+        if (usuario) {
+            return res.status(400).json({ message: 'El Usuario ya esta registrado' })
+        }
 
         usuario = new Usuario(req.body);
+        // hashear password, antes hacer un salt
+        const salt = await bcryptjs.genSalt(10)
+
+        // reescribir nuestro password
+        usuario.password = await bcryptjs.hash(password, salt);
 
         await usuario.save()
+
         res.send("Usuario guardado")
     } catch (error) {
         console.log(error)
--- routes/Usuario.js
+++ routes/Usuario.js
@@ -3,9 +3,16 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controller/usuarioController');
+// validaremos usando express-validator
+const { check } = require('express-validator');
 
 //api/usuarios
 router.post('/',
+    [
+        check('nombre', 'El Nombre es obligatorio').not().isEmpty(),
+        check('email', 'Agregar un email valido').isEmail(),
+        check('password', 'El password debe contener minimo 6 caracteres').isLength({ min: 6 })
+    ],
     usuarioController.crearUsuario
 )
 
