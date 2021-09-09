082603d698d24b59af37ae7dc897459a7213ec2f
alotav <alonsotaverna@hotmail.com>
2021-09-09 14:18:08 -0300

    clase 3

--- app.js
+++ app.js
@@ -2,18 +2,65 @@ const express = require('express');
 const app = express();
 const port = 3000; //app.listen(3000)
 
+
+// METODO GET
+
 app.get("/", (req, res) => {
     // res.send("Hola Clase PWA")
+    res.json(
+                {
+                    "status": "true",
+                    "message": "ok",
+                    "data": 
+                    [
+                        {
+                            id: 1,
+                            nombre: 'Homero',
+                            apellido: 'Simpson',
+                            edad: 35,
+                        },
+                        {
+                            id: 2,
+                            nombre: 'Bart',
+                            apellido: 'Simpson',
+                            edad: 8,
+                        },
+                        {
+                            id: 3,
+                            nombre: 'Lisa',
+                            apellido: 'Simpson',
+                            edad: 10,
+                        },
+                        {
+                            id: 4,
+                            nombre: 'Magui',
+                            apellido: 'Simpson',
+                            edad: 2,
+                        },
+                        {
+                            id: 5,
+                            nombre: 'Marge',
+                            apellido: 'Bouvier',
+                            edad: 35,
+                        },
+                    ]
+                }
+       
+            )
+});
+
+// METODO POST
+
+app.post('/', (req, res) =>{
     res.send(
         `
         <html>
             <head></head>
                 <body>
-                    <p> PAGINA DE INICIO </p>
+                    <p>POST PAGINA DE INICIO </p>
                 </body>
         </html>
-        `
-    )
+        `)
 });
 
 app.listen(port, () => {
